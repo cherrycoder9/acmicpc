1337787235ddabe25d3215f0a791e2fbafe8b7fa
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-21 15:35:48 +0900

    test

--- bronze/2588/2588.js
+++ bronze/2588/2588.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf8').trim();
-const [a, b] = input.split(' ');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const [a, b] = input.split('\n');
+const numA = Number(a);
+const numB = Number(b);
 
-console.log(+a * +b[0]);
-console.log(+a * +b[1]);
-console.log(+a * +b[2]);
-console.log(+a * +b);
+const b1 = Number(b[2]);
+const b2 = Number(b[1]);
+const b3 = Number(b[0]);
+
+const line1 = numA * b1;
+const line2 = numA * b2;
+const line3 = numA * b3;
+const line4 = numA * numB;
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+console.log(line4);
