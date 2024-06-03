26a24825eb28a7c09a206f20b15df21c3a50d115
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-03 21:58:36 +0900

    dfsgd

--- test/test.js
+++ test/test.js
@@ -1,13 +1,5 @@
+const countMap = new Map();
 
-let obj = { a: 'hi', b: 1234, c: 'hahahoho' };
-for (const mykey in obj) {
-    console.log(mykey, obj[mykey]);
-}
-
-let soso = [1, 3, 5, 7, 9, 11, 13];
-soso.forEach((element) => {
-    console.log(element);
-});
-for (const e of soso) {
-    console.log(e);
-}
+countMap.set('hi', 3);
+countMap.set('my', 9);
+console.log(countMap);
