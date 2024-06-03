66e742ed84c97452f7d6c83b8625d0bf59561840
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-03 09:34:26 +0900

    dsfsdf

--- bronze/1157/1157.js
+++ bronze/1157/1157.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim();
+const saveList = [];
 
 input = input.toUpperCase();
+input.forEach(element => {
+    for (element in saveList) {
+        if (saveList[element] == null) {
+            saveList[element] = 0;
+            break;
+        } else {
+            saveList[element]++;
+        }
+    }
+});
