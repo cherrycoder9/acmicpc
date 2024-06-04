91e18a92f8ca11a59ca11901166ede072f9e2f84
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-04 09:34:14 +0900

    sdaff

--- test/test.js
+++ test/test.js
@@ -1,7 +1,17 @@
-const countMap = new Map();
 
-// countMap.set('hi', 3);
-// countMap.set('my', 9);
-console.log(countMap);
-console.log(countMap.length);
-console.log(countMap.size);
+const map = new Map();
+map.set('a', 1);
+map.set('b', 2);
+map.set('c', 3);
+console.log(map);
+
+const iterator = map.keys();
+console.log(iterator);
+
+for (const key of iterator) {
+    console.log(key); // 'a', 'b', 'c'
+}
+
+const array = [1, 2, 3];
+const arrayIterator = array.keys();
+console.log(arrayIterator); // Array Iterator { 0, 1, 2 }
